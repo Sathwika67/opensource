3e85caaba8205710f80af35672aa43ab52bf6dcc
Sathwika67 <sathwikachikki67@gmail.com>
2024-11-16 08:53:57 +0530

    Create Season Game.py

---
+++ visaa_prep/Season Game.py
@@ -0,0 +1,11 @@
+n=int(input())
+if(3<=n<=5):
+    print("Spring")
+elif(6<=n<=8):
+    print("Summer")
+elif(9<=n<=11):
+    print("Autumn")
+elif(n>12):
+    print("Invalid")
+else:
+    print("Winter")
